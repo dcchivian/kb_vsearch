2c4830570747f7f17e1f4d2b9d1600df7502b9f9
Dylan Chivian <DCChivian@lbl.gov>
2016-03-10 15:33:02 -0800

    added query object creation for sequence input

--- lib/kb_vsearch/kb_vsearchImpl.py
+++ lib/kb_vsearch/kb_vsearchImpl.py
@@ -235,8 +235,10 @@ class kb_vsearch:
         #
         if 'workspace_name' not in params:
             raise ValueError('workspace_name parameter is required')
-        if 'input_one_name' not in params and 'input_one_sequence' not in params:
-            raise ValueError('input_one_sequence or input_one_name parameter is required')
+#        if 'input_one_name' not in params and 'input_one_sequence' not in params:
+#            raise ValueError('input_one_sequence or input_one_name parameter is required')
+        if 'input_one_name' not in params:
+            raise ValueError('input_one_name parameter is required')
         if 'input_many_name' not in params:
             raise ValueError('input_many_name parameter is required')
         if 'output_filtered_name' not in params:
@@ -248,22 +250,148 @@ class kb_vsearch:
         if 'input_one_sequence' in params \
                 and params['input_one_sequence'] != None \
                 and params['input_one_sequence'] != "Enter DNA sequence...":
-            input_one_file_name = 'user_query.fna'
+            input_one_file_name = params['input_one_name']
             one_forward_reads_file_path = os.path.join(self.scratch,input_one_file_name)
             one_forward_reads_file_handle = open(one_forward_reads_file_path, 'w', 0)
             self.log(console, 'writing query reads file: '+str(one_forward_reads_file_path))
 
-            input_sequence_buf = params['input_one_sequence'].split("\n")
-            one_forward_reads_file_handle.write('>UserQuery'+"\n")
-            query_line_seen = False
-            for line in input_sequence_buf:
-                if not line.startswith('>'):
-                    one_forward_reads_file_handle.write(line+"\n")
+#            input_sequence_buf = params['input_one_sequence'].split("\n")
+#            one_forward_reads_file_handle.write('>'+params['input_one_name']+"\n")
+#            query_line_seen = False
+#            for line in input_sequence_buf:
+#                if not line.startswith('>'):
+#                    one_forward_reads_file_handle.write(line+"\n")
+#                else:
+#                    if query_line_seen:
+#                        break
+#                    query_line_seen = True
+#            one_forward_reads_file_handle.close();
+
+            fastq_format = False
+            input_sequence_buf = params['input_one_sequence']
+            if input_sequence_buf.startswith('@'):
+                fastq_format = True
+                #self.log(console,"INPUT_SEQ BEFORE: '''\n"+input_sequence_buf+"\n'''")  # DEBUG
+                input_sequence_buf = re.sub ('&apos;', "'", input_sequence_buf)
+                input_sequence_buf = re.sub ('&quot;', '"', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#39;',  "'", input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#34;',  '"', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&lt;;',  '<', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#60;',  '<', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&gt;',   '>', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#62;',  '>', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#36;',  '$', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#37;',  '%', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#47;',  '/', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#63;',  '?', input_sequence_buf)
+##        input_sequence_buf = re.sub ('&#92;',  chr(92), input_sequence_buf)  # FIX LATER
+#        input_sequence_buf = re.sub ('&#96;',  '`', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#124;', '|', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&amp;', '&', input_sequence_buf)
+#        input_sequence_buf = re.sub ('&#38;', '&', input_sequence_buf)
+#        self.log(console,"INPUT_SEQ AFTER: '''\n"+input_sequence_buf+"\n'''")  # DEBUG
+
+                DNA_pattern = re.compile("^[acgtuACGTU ]+$")
+                space_pattern = re.compile("^[ \t]*$")
+                split_input_sequence_buf = input_sequence_buf.split("\n")
+
+                # no header rows, just sequence
+                if not input_sequence_buf.startswith('>') and not input_sequence_buf.startswith('@'):
+                    one_forward_reads_file_handle.write('>'+params['input_one_name']+"\n")
+                    for line in split_input_sequence_buf:
+                        if not space_pattern.match(line):
+                            line = re.sub (" ","",line)
+                            line = re.sub ("\t","",line)
+                            if not DNA_pattern.match(line):
+                                raise ValueError ("BAD record:\n"+line+"\n")
+                                sys.exit(0)
+                            one_forward_reads_file_handle.write(line.lower()+"\n")
+
                 else:
-                    if query_line_seen:
-                        break
-                    query_line_seen = True
-            one_forward_reads_file_handle.close();
+                    # format checks
+                    for i,line in enumerate(split_input_sequence_buf):
+                        if line.startswith('>') or line.startswith('@'):
+                            if not DNA_pattern.match(split_input_sequence_buf[i+1]):
+                                if fastq_format:
+                                    bad_record = "\n".join([split_input_sequence_buf[i],
+                                                    split_input_sequence_buf[i+1],
+                                                    split_input_sequence_buf[i+2],
+                                                    split_input_sequence_buf[i+3]])
+                                else:
+                                    bad_record = "\n".join([split_input_sequence_buf[i],
+                                                    split_input_sequence_buf[i+1]])
+                                raise ValueError ("BAD record:\n"+bad_record+"\n")
+                                sys.exit(0)
+                            if fastq_format and line.startswith('@'):
+                                format_ok = True
+                                seq_len = len(split_input_sequence_buf[i+1])
+                                if not seq_len > 0:
+                                    format_ok = False
+                                if not split_input_sequence_buf[i+2].startswith('+'):
+                                    format_ok = False
+                                if not seq_len == len(split_input_sequence_buf[i+3]):
+                                    format_ok = False
+                                if not format_ok:
+                                    bad_record = "\n".join([split_input_sequence_buf[i],
+                                                    split_input_sequence_buf[i+1],
+                                                    split_input_sequence_buf[i+2],
+                                                    split_input_sequence_buf[i+3]])
+                                    raise ValueError ("BAD record:\n"+bad_record+"\n")
+                                    sys.exit(0)
+
+                    # write that sucker, removing spaces
+                    #
+                    #forward_reads_file_handle.write(input_sequence_buf)        input_sequence_buf = re.sub ('&quot;', '"', input_sequence_buf)
+                    for i,line in enumerate(split_input_sequence_buf):
+                        if line.startswith('>'):
+                            split_input_sequence_buf[i+1] = re.sub (" ","",split_input_sequence_buf[i+1])
+                            split_input_sequence_buf[i+1] = re.sub ("\t","",split_input_sequence_buf[i+1])
+                            record = "\n".join([split_input_sequence_buf[i], \
+                                        split_input_sequence_buf[i+1].lower()]) \
+                                        + "\n"
+
+                            one_forward_reads_file_handle.write(record)
+                        elif line.startswith('@'):
+                            split_input_sequence_buf[i+1] = re.sub (" ","",split_input_sequence_buf[i+1])
+                            split_input_sequence_buf[i+1] = re.sub ("\t","",split_input_sequence_buf[i+1])
+                            split_input_sequence_buf[i+3] = re.sub (" ","",split_input_sequence_buf[i+3])
+                            split_input_sequence_buf[i+3] = re.sub ("\t","",split_input_sequence_buf[i+3])
+                            record = "\n".join([split_input_sequence_buf[i], \
+                                        split_input_sequence_buf[i+1].lower(), \
+                                        split_input_sequence_buf[i+2], \
+                                        split_input_sequence_buf[i+3]]) \
+                                        + "\n"
+                            one_forward_reads_file_handle.write(record)
+        
+            one_forward_reads_file_handle.close()
+
+
+            # load the method provenance from the context object
+            #
+            self.log(console,"SETTING PROVENANCE")  # DEBUG
+            provenance = [{}]
+            if 'provenance' in ctx:
+                provenance = ctx['provenance']
+            # add additional info to provenance here, in this case the input data object reference
+                provenance[0]['input_ws_objects'] = []
+                provenance[0]['service'] = 'kb_vsearch'
+                provenance[0]['method'] = 'VSearch_BasicSearch'
+
+                
+                # Upload results
+                #
+                self.log(console,"UPLOADING QUERY OBJECT")  # DEBUG
+
+                sequencing_tech = 'N/A'
+                self.upload_SingleEndLibrary_to_shock_and_ws (ctx,
+                                                      console,  # DEBUG
+                                                      params['workspace_name'],
+                                                      params['input_one_name'],
+                                                      one_forward_reads_file_path,
+                                                      provenance,
+                                                      sequencing_tech
+                                                      )
+
             self.log(console, 'done')
 
         else:  # obtain object
